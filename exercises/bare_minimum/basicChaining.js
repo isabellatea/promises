be95e35421702b2c453a69e9c9f399c4f5f5c513
Isabella Tea <tea.isabella@gmail.com>
2017-08-02 20:36:31 -0700

    completed basic requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -11,10 +11,20 @@
 var fs = require('fs');
 var Promise = require('bluebird');
 
-
+var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
+var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
+Promise.promisifyAll(fs);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+  .then(getGitHubProfileAsync)
+  .then(function(body) {
+  	return fs.writeFileAsync(writeFilePath, JSON.stringify(body), 'utf8');
+  })
+  .catch(function(error) {
+  	console.log(error);
+  })
 };
 
 // Export these functions so we can test them
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,32 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, 'utf8', function(error, response) {
+    	if (error) {
+    		reject(error);
+    	} else {
+    		var firstLine = response.split('\n')[0];
+    		resolve(firstLine);
+    	}
+    })
+
+
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise(function(resolve, reject) {
+  	request(url, function(error, response) {
+  		if (error) {
+  			reject(error);
+  		} else {
+  			resolve(response.statusCode);
+  		}
+  	})
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
